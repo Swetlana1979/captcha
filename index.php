f890f7a62ce3290505a9142ef6ee851d2bf23e4b
Светлана Плотникова <uzer1874@rambler.ru>
2021-06-17 09:43:57 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+// PHP программа для создания капчи с помощью  ImageMagick
+
+function createCaptcha(){
+	// новый объект 
+	$Imagick = new Imagick();
+	// класс ImagickPixel 
+	$bg = new ImagickPixel();
+	// Цвет фона
+	$bg->setColor( 'white' );
+	// прямоугольная форма капчи
+	$ImagickDraw = new ImagickDraw();
+	// размер шрифта
+	$ImagickDraw->setFontSize( 30 );
+	//Выводимый текст
+	$num = '0123456789';
+	$string = substr( str_shuffle( $num ), 2, 5);
+	//основа капчи с белым фоном
+	$Imagick->newImage( 100, 50, $bg ); 
+	//помещает текст в капчу
+	$Imagick->annotateImage( $ImagickDraw, 4, 30, 0, $string );
+	//центрирование
+	$Imagick->swirlImage( 30 );
+	// фоновые линиии
+	$i=5;
+	while($i>0){
+		$ImagickDraw->line( rand( 0, 100 ), rand( 0, 50 ), rand( 0, 100 ), rand( 0, 50 ) );
+		$i=$i-1;
+	}
+	//Объединение фона в один рисунок
+	$Imagick->drawImage( $ImagickDraw );
+	//формат
+	$Imagick->setImageFormat( 'png' );
+	//отправка заголовков и вывод капчи
+	header( "Content-Type: image/{$Imagick->getImageFormat()}" );
+	echo $Imagick->getImageBlob( );
+}
+createCaptcha();
+?>
