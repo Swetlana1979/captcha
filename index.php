2d1fab13239140ff181f8c52a78b68df3df7da71
Светлана Плотникова <uzer1874@rambler.ru>
2025-01-05 23:30:35 +0400

    index.php

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 // PHP программа для создания капчи с помощью  ImageMagick
 
 function createCaptcha(){
-	// новый объект 
+	
 	$Imagick = new Imagick();
-	// класс ImagickPixel 
 	$bg = new ImagickPixel();
 	// Цвет фона
 	$bg->setColor( 'white' );
@@ -17,8 +16,10 @@ function createCaptcha(){
 	$string = substr( str_shuffle( $num ), 2, 5);
 	//основа капчи с белым фоном
 	$Imagick->newImage( 100, 50, $bg ); 
+	
 	//помещает текст в капчу
 	$Imagick->annotateImage( $ImagickDraw, 4, 30, 0, $string );
+	
 	//центрирование
 	$Imagick->swirlImage( 30 );
 	// фоновые линиии
@@ -27,6 +28,7 @@ function createCaptcha(){
 		$ImagickDraw->line( rand( 0, 100 ), rand( 0, 50 ), rand( 0, 100 ), rand( 0, 50 ) );
 		$i=$i-1;
 	}
+	
 	//Объединение фона в один рисунок
 	$Imagick->drawImage( $ImagickDraw );
 	//формат
